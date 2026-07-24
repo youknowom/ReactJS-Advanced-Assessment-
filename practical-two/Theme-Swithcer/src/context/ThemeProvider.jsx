c76bfc80db06d49c0b66acf2aa966c1de54ee2ff
Omkaar <omdb2004@gmail.com>
2026-07-24 21:17:31 +0530

    Implement theme switching functionality with context provider and CSS updates

---
+++ practical-two/Theme-Swithcer/src/context/ThemeProvider.jsx
@@ -0,0 +1,22 @@
+import { useMemo, useState } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  };
+
+  const value = useMemo(
+    () => ({
+      theme,
+      toggleTheme,
+    }),
+    [theme],
+  );
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
